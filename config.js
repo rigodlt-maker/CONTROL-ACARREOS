6cd657a29284236b4abc162e463bca5c142d0da9
rigodlt-maker <rigo.dlt@gmail.com>
2026-06-25 11:55:35 -0600

    Update Firebase configuration in config.js
    Updated Firebase configuration with new credentials.

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyADUbA3X07prA5yOUzFJX_zVntN88-JMwc",
+  authDomain: "acarreos-rompeolas.firebaseapp.com",
+  projectId: "acarreos-rompeolas",
+  storageBucket: "acarreos-rompeolas.firebasestorage.app",
+  messagingSenderId: "217225530978",
+  appId: "1:217225530978:web:f742aa093a972401ce4b3b"
+};
